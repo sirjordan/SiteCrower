5c3ce29b07883f10b7caa43bebc4442c39f43ac1
Martin <sirjordan1988@gmail.com>
2018-01-11 22:57:04 +0200

    i
    i

--- SiteCrower.Core/LinkDispatcher.cs
+++ SiteCrower.Core/LinkDispatcher.cs
@@ -5,7 +5,11 @@ namespace SiteCrower.Core
 {
     public class LinkDispatcher
     {
+        private static readonly string validUrlPattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
+
         private readonly string root;
+        private readonly string http = "http://";
+        private readonly string https = "https://";
 
         public LinkDispatcher(string root)
         {
@@ -19,34 +23,34 @@ namespace SiteCrower.Core
 
         public static bool IsUrlValid(string url)
         {
-            string pattern = @"^(http|https|ftp|)\://|[a-zA-Z0-9\-\.]+\.[a-zA-Z](:[a-zA-Z0-9]*)?/?([a-zA-Z0-9\-\._\?\,\'/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$";
-            Regex reg = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            Regex reg = new Regex(validUrlPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
             return reg.IsMatch(url);
         }
         
         public string GetDomain(string rawUrl)
         {
-            if (rawUrl.StartsWith("/") || (!rawUrl.ToLower().StartsWith("http://") && !rawUrl.ToLower().StartsWith("https://"))) 
+            if (rawUrl.StartsWith("/") || (!rawUrl.ToLower().StartsWith(http) && !rawUrl.ToLower().StartsWith(https))) 
             {
                 return GetDomain(root);
             }
 
             Uri uri = new Uri(rawUrl);
             string url = uri.Host.ToString();
+
             return url;
         }
 
         public string DecorateUrl(string url)
         {
-            if (url.StartsWith("/") || (!url.ToLower().StartsWith("http://") && !url.ToLower().StartsWith("https://")))
+            if (url.StartsWith("/") || (!url.ToLower().StartsWith(http) && !url.ToLower().StartsWith(https)))
             {
                 string separator = url.StartsWith("/") ? "" : "/";
                 url = string.Format("{0}{1}{2}", GetDomain(root), separator, url);
             }
 
-            if (!url.ToLower().StartsWith("http://"))
+            if (!url.ToLower().StartsWith(http))
             {
-                string prefix = "http://";
+                string prefix = http;
                 if (url.StartsWith("/"))
                 {
                     prefix = "http:/";
