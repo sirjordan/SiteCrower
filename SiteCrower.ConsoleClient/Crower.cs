71fe1714463547b94d9001603976c58db2bb9232
Martin <sirjordan1988@gmail.com>
2018-01-11 23:46:34 +0200

    updgrades
    updgrades

--- SiteCrower.ConsoleClient/Crower.cs
+++ SiteCrower.ConsoleClient/Crower.cs
@@ -1,17 +1,25 @@
 ﻿using SiteCrower.Core;
 using System;
+using System.Text;
 
 namespace SiteCrower.ConsoleClient
 {
     class Crower
     {
+        static string siteRoot;
+        static int urlProcessed = 0;
+        static DateTime start;
+
         static void Main()
         {
+            start = DateTime.Now;
+
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.Write("Site Root:");
-            string root = Console.ReadLine();
 
-            var requestProcessor = new RequestProcessor(root);
+            siteRoot = Console.ReadLine();
+
+            var requestProcessor = new RequestProcessor(siteRoot);
             requestProcessor.RequestProceed += RequestProcessor_RequestRequestProceed;
             requestProcessor.Start();
         }
@@ -19,7 +27,15 @@ namespace SiteCrower.ConsoleClient
         private static void RequestProcessor_RequestRequestProceed(object sender, ProcessResult e)
         {
             Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write(e.Url);
+            Console.Clear();
+
+            Console.WriteLine(siteRoot);
+            Console.WriteLine();
+
+            Console.WriteLine($"Url processed: {++urlProcessed} @ {string.Format("{0:0.0}", (DateTime.Now - start).TotalSeconds)} seconds");
+            Console.WriteLine();
+
+            Console.Write($"Processing: {e.Url}");
 
             switch (e.Status)
             {
